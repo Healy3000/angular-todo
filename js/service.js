e3e8fed6c3c32efa43fd9388886f73a9f938de1f
Edward <Edward.francis.healy@outlook.com>
2017-01-31 19:11:34 +0000

    k

--- js/service.js
+++ js/service.js
@@ -1,13 +1,12 @@
 angular.module('UserService', [])
-    .factory('UserAPIService', function($http) {
- 
-        UserAPIService = {
-            callAPI: function(url, data) {
-                return $http.post(url, data)
-            }
-        }
-        return UserAPIService;
-    });
+	.factory('UserAPIService', function($http) {
+		UserAPIService = {
+			callAPI: function(url, data) {
+				return $http.post(url, data);
+			}
+		};
+		return UserAPIService;
+	});
 	
 	angular.module('TodoService', [])
     .factory('TodoAPIService', function($http) {
@@ -15,6 +14,21 @@ angular.module('UserService', [])
             getTodos: function(url, data, token) {
                 var header = "Authorization: JWT " + token;
                 return $http.get(url, {params:{"username": data}}, header);
+            }
+        };
+        return TodoAPIService;
+    });
+	
+	angular.module('TodoService', [])
+    .factory('TodoAPIService', function($http) {
+        TodoAPIService = {
+            getTodos: function(url, data, token) {
+                header = "Authorization: JWT " + token;
+                return $http.get(url, {params:{"username": data}}, header);
+            },
+            createTodo: function(url, data, token) {
+                header = "Authorization: JWT " + token;
+                return $http.post(url, data, header);
             }
         };
         return TodoAPIService;
